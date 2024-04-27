7718be178d6818483ecf9b86b74c7053f1483864
Nikita <katshido.2003@gmail.com>
2024-04-27 22:56:13 +0300

    frontend: create the header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Cvs from "./pages/Cvs";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cvs" element={<Cvs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ frontend/src/pages/Cvs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Cvs() {
+  return (
+    <div>Cvs</div>
+  )
+}
